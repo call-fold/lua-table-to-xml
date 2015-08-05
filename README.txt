84347aad92ade5d9cb20f3044fc52fe7be5afc81
call-fold <568925701@qq.com>
2015-08-05 17:51:06 +0800

    按lua格式修改程序

---
+++ README.txt
@@ -0,0 +1,8 @@
+xml_generate.bat---生成需要的xml文件
+xml_delete.bat---删除生成的xml文件
+table_define.lua---需要填的量放在里面
+lua---lua解释器
+
+@author: 苏铃峰
+@company: ONEKES
+@data: 2015/08/05
--- xml_generate.lua
+++ xml_generate.lua
@@ -1,18 +1,18 @@
--- =================================从table_define.lua中取出需要的表和表名
+-------------------------------------------------------------------------------------------------------
+-- xml映射表生成小工具
+-- @author 苏铃峰
+-- @copyright ONEKES
+-- @release common.lua, v5.3.1 2015/08/05
+-------------------------------------------------------------------------------------------------------
 
-local originTable = dofile("table_define.lua")
+-------------------------------------------------------------------------------------------------------
+-- 按格式写XML文件
 
-for fileName_,myTable_ in next, originTable do
-	local fileName = fileName_
-	local myTable ={}
-	for k,v in next, myTable_ do
-		myTable[k] = v
-	end
+local fileName 		--新建xml文件名
+local myTable = {} 	--新建读内容的表
+local file 		--读表时，需要打开的文件
 
-	-- ================================写xml文件
-
-	local file = io.open(fileName..".xml", "w")
-
+local function writeXML(  )
 	local function writeTitle(  )	--写文件头部
 		file:write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>")
 		file:write("\n")
@@ -40,17 +40,37 @@ for fileName_,myTable_ in next, originTable do
 		file:write("\n")
 	end
 
-	-- ================================主函数
-
-	function main(  )
-		writeTitle()
-		local LOOP_TEXT = 2
-		for i=1, LOOP_TEXT do
-			writeText()	--按格式需要调用两次
-		end
-		writeEnd()
-		file:close()
+	writeTitle()
+	local LOOP_TEXT = 2
+	for i=1, LOOP_TEXT do
+		writeText()	--按格式需要调用两次
 	end
-
-	main()
+	writeEnd()
 end
+
+-------------------------------------------------------------------------------------------------------
+-- 读取table_define.lua中表头和表的内容，同时在新建xml文件中加入写的内容
+
+local function getTable(  )
+	local originTable = dofile("table_define.lua")
+	for fileName_,myTable_ in next, originTable do
+		fileName = fileName_
+		for k,v in next, myTable_ do
+			myTable[k] = v
+		end
+
+		file = io.open(fileName..".xml", "w")
+		writeXML()
+		file:close()
+
+	end
+end
+
+-------------------------------------------------------------------------------------------------------
+-- 主函数
+
+local function main(  )
+	getTable()
+end
+
+main()
